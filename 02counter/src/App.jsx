0371df6bb1636005888da14cc4a9fe97dfdf5c67
Ankush Maurya <ankushmaurya881@gmail.com>
2024-02-11 20:11:40 +0530

    fifth-commit

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -5,33 +5,26 @@ import "./App.css";
 
 function App() {
   // let counter = 5;
-
   //Can give any default values '',{},0,etc.
-
   //counter is a variable and setCounter is a function which is used to change the values after an action has been made.
+
   let [counter, setCounter] = useState(5);
 
   const addValue = () => {
     console.log("Clicked", Math.random());
     // counter = counter + 1;
-    if(counter<20){
+    if (counter < 20) {
       setCounter(counter + 1);
 
       console.log(counter);
     }
-
-
   };
 
   const removeValue = () => {
-    if(counter>0){
-
-      setCounter(counter-1);
-
+    if (counter > 0) {
+      setCounter(counter - 1);
       console.log(counter);
     }
-
-   
   };
   return (
     <>
@@ -39,12 +32,10 @@ function App() {
       <h2>
         {counter} Value: {counter}
       </h2>
-
       <button onClick={addValue}>Add value {counter}</button>
       <button onClick={removeValue}>Subtract value {counter}</button>
       <p>footer: {counter}</p>
     </>
   );
 }
-
 export default App;
