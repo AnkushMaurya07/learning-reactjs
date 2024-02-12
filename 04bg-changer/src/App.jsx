560dc3fd2868e466ecfd4e3a7547c5ea238a4786
Ankush Maurya <ankushmaurya881@gmail.com>
2024-02-12 19:19:31 +0530

    seventh-updated

--- 04bg-changer/src/App.jsx
+++ 04bg-changer/src/App.jsx
@@ -2,10 +2,82 @@
 import { useState } from "react";
 
 function App() {
-  const [color, setColor] = useState("olive");
+  const [color, setColor] = useState("green");
+  const [top, setTop] = useState("orangered");
 
   return (
     <>
+      <div
+        className="flex justify-center py-16  text-black space-x-3"
+        style={{ backgroundColor: top }}
+      >
+        <button
+          onClick={() => setTop("red")}
+          className="outline-none px-4 rounded-full text-white small"
+          style={{ backgroundColor: "red" }}
+        >
+          Red
+        </button>
+        <button
+          onClick={() => setTop("green")}
+          className="outline-none px-4 rounded-full text-white small"
+          style={{ backgroundColor: "green" }}
+        >
+          Green
+        </button>
+        <button
+          onClick={() => setTop("blue")}
+          className="outline-none px-4 rounded-full text-white small"
+          style={{ backgroundColor: "blue" }}
+        >
+          Blue
+        </button>
+        <button
+          onClick={() => setTop("black")}
+          className="outline-none px-4 rounded-full text-white small"
+          style={{ backgroundColor: "black" }}
+        >
+          Black
+        </button>
+        <button
+          onClick={() => setTop("gray")}
+          className="outline-none px-4 rounded-full text-white small"
+          style={{ backgroundColor: "gray" }}
+        >
+          Gray
+        </button>{" "}
+        <button
+          onClick={() => setTop("violet")}
+          className="outline-none px-4 rounded-full text-white small"
+          style={{ backgroundColor: "violet" }}
+        >
+          Violet
+        </button>{" "}
+        <button
+          onClick={() => setTop("purple")}
+          className="outline-none px-4 rounded-full text-white small"
+          style={{ backgroundColor: "purple" }}
+        >
+          Purple
+        </button>
+        <button
+          onClick={() => setTop("Limegreen")}
+          className="outline-none px-4 rounded-full text-white small"
+          style={{ backgroundColor: "Limegreen" }}
+        >
+          Limegreen
+        </button>
+      </div>
+      <div
+        className="h-50 w-full flex justify-center bg-white"
+        style={{ backgroundColor: "" }}
+      >
+        <img
+          className="w-56 h-40 flex justify-center"
+          src="https://cdn.pixabay.com/photo/2023/05/13/16/31/ashok-chakra-7990878_1280.png"
+          alt="Bike image was here"
+        />
+      </div>
       <div
         className="w-screen h-screen duration-200 "
         style={{ backgroundColor: color }}
