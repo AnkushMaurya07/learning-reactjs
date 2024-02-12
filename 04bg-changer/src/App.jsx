c03f5a8ceefe5a3355e40d5218879d84e597a0fb
Ankush Maurya <ankushmaurya881@gmail.com>
2024-02-12 18:48:13 +0530

    seventh-commit

---
+++ 04bg-changer/src/App.jsx
@@ -0,0 +1,78 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+
+function App() {
+  const [color, setColor] = useState("olive");
+
+  return (
+    <>
+      <div
+        className="w-screen h-screen duration-200 "
+        style={{ backgroundColor: color }}
+      >
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+          <div className="flex flex-wrap justofy-center gap-3 shadow-xl bg-white px-3 py-2 rounded-3xl">
+            <button
+              onClick={() => setColor("red")}
+              className="outline-none px-4 rounded-full text-white small"
+              style={{ backgroundColor: "red" }}
+            >
+              Red
+            </button>
+            <button
+              onClick={() => setColor("green")}
+              className="outline-none px-4 rounded-full text-white small"
+              style={{ backgroundColor: "green" }}
+            >
+              Green
+            </button>
+            <button
+              onClick={() => setColor("blue")}
+              className="outline-none px-4 rounded-full text-white small"
+              style={{ backgroundColor: "blue" }}
+            >
+              Blue
+            </button>
+            <button
+              onClick={() => setColor("black")}
+              className="outline-none px-4 rounded-full text-white small"
+              style={{ backgroundColor: "black" }}
+            >
+              Black
+            </button>
+            <button
+              onClick={() => setColor("gray")}
+              className="outline-none px-4 rounded-full text-white small"
+              style={{ backgroundColor: "gray" }}
+            >
+              Gray
+            </button>{" "}
+            <button
+              onClick={() => setColor("yellow")}
+              className="outline-none px-4 rounded-full text-white small"
+              style={{ backgroundColor: "yellow" }}
+            >
+              Yellow
+            </button>{" "}
+            <button
+              onClick={() => setColor("purple")}
+              className="outline-none px-4 rounded-full text-white small"
+              style={{ backgroundColor: "purple" }}
+            >
+              Purple
+            </button>
+            <button
+              onClick={() => setColor("Limegreen")}
+              className="outline-none px-4 rounded-full text-white small"
+              style={{ backgroundColor: "Limegreen" }}
+            >
+              Limegreen
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
