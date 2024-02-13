09dccd129a86fd05f7e61a1e88b749191bf296a1
Ankush Maurya <ankushmaurya881@gmail.com>
2024-02-13 23:34:13 +0530

    eighth-commit

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -2,24 +2,19 @@ import { useState } from "react";
 
 import "./App.css";
 
-async function App() {
+function App() {
   // let counter = 5;
   //Can give any default values '',{},0,etc.
   //counter is a variable and setCounter is a function which is used to change the values after an action has been made.
-
   let [counter, setCounter] = useState(5);
 
   const addValue = () => {
     console.log("Clicked", Math.random());
     // counter = counter + 1;
-
-    
     if (counter < 20) {
       setCounter(counter + 1);
       // console.log(counter);
-
       setCounter((counter) => counter + 1);
-
       console.log(counter);
     }
   };
@@ -41,7 +36,5 @@ async function App() {
       <p>footer: {counter}</p>
     </>
   );
-
-  
 }
 export default App;
