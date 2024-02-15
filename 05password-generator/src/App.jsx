33b1fe471bc450ac9f04081c001cc0399fbc8781
Ankush Maurya <ankushmaurya881@gmail.com>
2024-02-15 10:15:47 +0530

    eight-complete

--- 05password-generator/src/App.jsx
+++ 05password-generator/src/App.jsx
@@ -1,36 +1,105 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect, useRef } from "react";
 
 import "./App.css";
 
 function App() {
-  const [length, setLength] = useState(0);
+  const [length, setLength] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState(""); //setPassword(method)
 
+  //useRef hook
+  const passwordRef = useRef(null);
+
+  //for memoisation
   const passwordGenerator = useCallback(() => {
     let pass = "";
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-
     if (numberAllowed) str += "0123456789";
     if (charAllowed) str += "!@#%%^&*()_+-[]{}`~";
 
-    for (let i = 1; i <= array.length; i++) {
-      let char = Math.random() * str.length + 1;
-      pass = str.charAt(char);
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
     }
 
     setPassword(pass);
   }, [length, numberAllowed, charAllowed, setPassword]);
 
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numberAllowed, charAllowed, passwordGenerator]);
+
+  const copyPasswordToClipboard = useCallback(() => {
+    //Selects the whole section which is being copied
+    passwordRef.current?.select();
+
+    //Selects the section within the range described/given
+    passwordRef.current?.setSelectionRange(0, 100);
+
+    //window object present in react(Not in nextjs)
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+
   return (
     <>
-      <h1 className="text-4xl text-center">Password Generator</h1>
+      {/* <h1 className="text-4xl text-center my-4">Password Generator</h1> */}
 
-      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-400">
-        Test
+      <div className="w-full max-w-max mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+        <h1 className="text-white text-center my-3">Password Generator</h1>
         <div className="flex shadow rounded-lg overflow-hidden mb-4">
-          <input type="text" value={password} placeholder="" />
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="password"
+            readOnly
+            ref={passwordRef}
+          />
+          <button
+            onClick={copyPasswordToClipboard}
+            className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 rounded-md"
+          >
+            Copy
+          </button>
+          <div className="flex text-sm gap-x-2">
+            <div className="flex text-center gap-x-1">
+              <input
+                type="range"
+                min={6}
+                max={100}
+                value={length}
+                className="cursor-pointer"
+                onChange={(e) => {
+                  setLength(e.target.value);
+                }}
+              />
+              <label>Length:{length}</label>
+            </div>
+
+            <div className="flex text-center gap-x-1">
+              <input
+                type="checkbox"
+                defaultChecked={numberAllowed}
+                id="numberInput"
+                onChange={() => {
+                  setNumberAllowed((prev) => !prev);
+                }}
+              />
+              <label htmlFor="numberInput">Number</label>
+            </div>
+            <div className="flex text-center gap-x-1">
+              <input
+                type="checkbox"
+                defaultChecked={numberAllowed}
+                id="numberInput"
+                onChange={() => {
+                  setNumberAllowed((prev) => !prev);
+                }}
+              />
+              <label htmlFor="characterInput">Characters</label>
+            </div>
+          </div>
         </div>
       </div>
     </>
