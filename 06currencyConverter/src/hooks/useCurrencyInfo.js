58eb4e708d6fe8618f64169b41d80e9880c32635
Ankush Maurya <ankushmaurya881@gmail.com>
2024-02-19 17:13:18 +0530

    ninth-proj

---
+++ 06currencyConverter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react"
+
+function useCurrencyInfo(currency) {
+
+    const [data, setData] = useState({})
+    //using useEffect to load and whenever the function is called it calls the api directly.
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+
+            //converting into json
+            .then((res) => res.json)
+            //hold the data in the json format
+            .then((res) => setData(res[currency]))
+        console.log(data);
+        return data
+    }, [currency])
+    console.log(data);
+    return data
+}
+
+export default useCurrencyInfo;
