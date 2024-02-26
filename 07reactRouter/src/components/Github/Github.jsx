79b46179d444afcec293974a2a8a29603fc79e25
Ankush Maurya <ankushmaurya881@gmail.com>
2024-02-26 14:02:00 +0530

    router-proj

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
@@ -32,3 +33,39 @@ export const githubInfoLoader = async () => {
   const response=await fetch("https://api.github.com/users/AnkushMaurya07");
   return response.json()
 };
+=======
+import React, { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+
+function Github() {
+
+    const data=useLoaderData()
+
+//   const [data, setData] = useState([]);
+//   useEffect(() => {
+//     fetch("https://api.github.com/users/AnkushMaurya07")
+//       .then((response) => response.json())
+//       .then((data) => {
+//         console.log(data);
+//         setData(data);
+//       });
+//   }, []);
+  return (
+    <div className="text-center bg-blue-100 text-black text-5xl pt-5 flex justify-center ">
+      Github Followers:{data.followers}
+      <img
+        className=" rounded-full "
+        src={data.avatar_url}
+        alt="Git picture"
+      ></img>
+    </div>
+  );
+}
+
+export default Github;
+
+export const githubInfoLoader = async () => {
+  const response=await fetch("https://api.github.com/users/AnkushMaurya07");
+  return response.json()
+};
+>>>>>>> 932e2d8 (router-proj)
--- 07reactRouter/src/components/User/User.jsx
+++ 07reactRouter/src/components/User/User.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import { useParams } from "react-router-dom";
 
@@ -9,3 +10,16 @@ function User() {
 }
 
 export default User;
+=======
+import React from "react";
+import { useParams } from "react-router-dom";
+
+function User() {
+  const { userid } = useParams();
+  return (
+  <div className="bg-blue-600 rounded-full text-4xl pt-7m">User:{userid}</div>
+  )
+}
+
+export default User;
+>>>>>>> 932e2d8 (router-proj)
